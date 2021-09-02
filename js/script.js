c794d6414aedc9d49c3f2c826489413f98d83258
Roman Pytalev <roman@pytalev.com>
2021-09-03 02:13:32 +0300

    Refactoring Pytalev.html

--- js/script.js
+++ js/script.js
@@ -7,6 +7,28 @@ document.querySelector('.hamburger').addEventListener('click', function() {
 
 window.addEventListener('scroll', function() {
   document.querySelector('.hamburger').style.position = 'absolute';
+  var buttonUpWrapper = document.querySelector('.buttonUp-wrapper');
+  var buttonUp = document.querySelector('.buttonUp');
+  if(pageYOffset >= document.documentElement.clientHeight / 2) {
+    buttonUpWrapper.style.visibility = "visible";
+    buttonUp.style.visibility = "visible";
+    document.querySelector('.buttonUp').addEventListener('mouseenter', function() {
+        buttonUpWrapper.style.backgroundColor = '#ff7800';
+        buttonUp.style.borderColor = 'white';
+    });
+    document.querySelector('.buttonUp').addEventListener('mouseleave', function() {
+        buttonUpWrapper.style.backgroundColor = 'white';
+        buttonUp.style.borderColor = '#ff7800';
+    });
+    document.querySelector('.buttonUp').addEventListener('click', function() {
+      window.scrollTo(pageXOffset, 0);
+      buttonUpWrapper.style.visibility = "hidden";
+      buttonUp.style.visibility = "hidden";
+    });
+  } else {
+    buttonUpWrapper.style.visibility = "hidden";
+    buttonUp.style.visibility = "hidden";
+  }
 });
 
 function mouseenterButtonPrev() {
@@ -36,84 +58,3 @@ function mouseleaveButtonNext() {
   buttonNextWrapper.style.backgroundColor = 'white';
   buttonNext.style.borderColor = '#ff7800';
 }
-
-function mouseenterStudioButtonUp() {
-  var studioButtonUpWrapper = document.querySelector('.studio-button-wrapper');
-  var studioButtonUp = document.querySelector('.up');
-  studioButtonUpWrapper.style.backgroundColor = '#ff7800';
-  studioButtonUp.style.borderColor = 'white';
-}
-      
-function mouseleaveStudioButtonUp() {
-  var studioButtonUpWrapper = document.querySelector('.studio-button-wrapper');
-  var studioButtonUp = document.querySelector('.up');
-  studioButtonUpWrapper.style.backgroundColor = 'white';
-  studioButtonUp.style.borderColor = '#ff7800';
-  if(pageYOffset < document.documentElement.clientHeight/2) {
-    studioButtonUp.style.borderColor = 'white';
-  }
-}
-
-function mouseenterPytalevButtonUp() {
-  var pytalevButtonUpWrapper = document.querySelector('.pytalev-button-wrapper');
-  var pytalevButtonUp = document.querySelector('.up');
-  pytalevButtonUpWrapper.style.backgroundColor = '#ff7800';
-  pytalevButtonUp.style.borderColor = 'white';
-}
-      
-function mouseleavePytalevButtonUp() {
-  var pytalevButtonUpWrapper = document.querySelector('.pytalev-button-wrapper');
-  var pytalevButtonUp = document.querySelector('.up');
-  pytalevButtonUpWrapper.style.backgroundColor = 'white';
-  pytalevButtonUp.style.borderColor = '#ff7800';
-  if(pageYOffset < document.documentElement.clientHeight/2) {
-    pytalevButtonUp.style.borderColor = 'white';
-  }
-}
-
-function clickStudioButtonUp() {
-  window.scrollTo(pageXOffset, 0);
-  };
-  window.addEventListener('scroll', function() {
-    var studioPageButtonUp = document.querySelector('.up');
-    if(pageYOffset > document.documentElement.clientHeight/2) {
-      studioPageButtonUp.style.borderColor = '#ff7800';
-    } else {
-      studioPageButtonUp.style.borderColor = 'white';
-    }
-  var logo = document.querySelector('.logo'); 
-  var menu = document.querySelector('.menu'); 
-  // var front = document.querySelector('.logo-cube__cube-side-front');
-  // var back = document.querySelector('.logo-cube__cube-side-back');
-  // var right = document.querySelector('.logo-cube__cube-side-right');
-  // var left = document.querySelector('.logo-cube__cube-side-left');
-  // var top = document.querySelector('.logo-cube__cube-side-top');
-  // var bottom = document.querySelector('.logo-cube__cube-side-bottom');
-  // var logoWhite = document.querySelector('#img-logo-color-white');
-  // var logoOrange = document.querySelector('#img-logo-color-orange')
-  var header = document.querySelector('.header__wrapper');
-  var coordsHeader = header.getBoundingClientRect();
-  var coordsLogo = logoWhite.getBoundingClientRect();
-  
-    if(coordsLogo.bottom < coordsHeader.bottom) {
-      // front.style.borderColor = '#ff7800';
-      // back.style.borderColor = '#ff7800';
-      // right.style.borderColor = '#ff7800';
-      // left.style.borderColor = '#ff7800';
-      // top.style.borderColor = '#ff7800';
-      // bottom.style.borderColor = '#ff7800';
-      // logoOrange.hidden = false;
-      // logoWhite.style.visibility = 'hidden';
-      logo.style.transform = 'translateX(300px)';
-    } else {
-        // front.style.borderColor = 'white';
-        // back.style.borderColor = 'white';
-        // right.style.borderColor = 'white';
-        // left.style.borderColor = 'white';
-        // top.style.borderColor = 'white';
-        // bottom.style.borderColor = 'white';
-        // logoWhite.style.visibility = 'visible';
-        // logoOrange.hidden = true;
-        logo.style.transform = 'translateX(0px)';
-        }
-});
