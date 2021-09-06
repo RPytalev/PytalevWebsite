7b2c2bd7122d8592fcdf9b0624673d9cd74bf269
Roman Pytalev <roman@pytalev.com>
2021-09-06 04:53:31 +0300

    Created responsive design, fixed code for buttonUp, refactoring all code according to BEM methodology web dev

--- app/js/script.js
+++ app/js/script.js
@@ -1,64 +1,68 @@
 'use strict'
-document.querySelector('.hamburger').addEventListener('click', function() {
+window.addEventListener('scroll', function() {
+  document.querySelector('.hamburger').style.position = 'absolute';
+    var buttonUp = document.querySelector('.main__buttonUp');
+    var scrolled = window.pageYOffset;
+    var height = document.documentElement.clientHeight;
+    if (scrolled > height / 5) {
+      buttonUp.classList.add('showButtonUp');
+    }
+    if (scrolled < height / 5) {
+      buttonUp.classList.remove('showButtonUp');
+    }
+});
 
+document.querySelector('.hamburger').addEventListener('click', function() {
   document.querySelector('.hamburger-menu').classList.toggle('firstChange');
   document.querySelector('.hamburger-menu__list').classList.toggle('secondChange');
 });
 
-window.addEventListener('scroll', function() {
-  document.querySelector('.hamburger').style.position = 'absolute';
-});
-
 function mouseenterButtonPrev() {
-  var buttonPrevWrapper = document.querySelector('.slider__button-prev');
-  var buttonPrev = document.querySelector('.slider__button-marker-arrow-left');
+  var buttonPrevWrapper = document.querySelector('.main__slider-button-prev');
+  var buttonPrev = document.querySelector('.main__slider-button-marker-arrow-left');
   buttonPrevWrapper.style.backgroundColor = '#ff7800';
   buttonPrev.style.borderColor = 'white';
   }
   
 function mouseleaveButtonPrev() {
-  var buttonPrevWrapper = document.querySelector('.slider__button-prev');
-  var buttonPrev = document.querySelector('.slider__button-marker-arrow-left');
+  var buttonPrevWrapper = document.querySelector('.main__slider-button-prev');
+  var buttonPrev = document.querySelector('.main__slider-button-marker-arrow-left');
   buttonPrevWrapper.style.backgroundColor = 'white';
   buttonPrev.style.borderColor = '#ff7800';
 }
 
 function mouseenterButtonNext() {
-  var buttonNextWrapper = document.querySelector('.slider__button-next');
-  var buttonNext = document.querySelector('.slider__button-marker-arrow-right');
+  var buttonNextWrapper = document.querySelector('.main__slider-button-next');
+  var buttonNext = document.querySelector('.main__slider-button-marker-arrow-right');
   buttonNextWrapper.style.backgroundColor = '#ff7800';
   buttonNext.style.borderColor = 'white';
 }
     
 function mouseleaveButtonNext() {
-  var buttonNextWrapper = document.querySelector('.slider__button-next');
-  var buttonNext = document.querySelector('.slider__button-marker-arrow-right');
+  var buttonNextWrapper = document.querySelector('.main__slider-button-next');
+  var buttonNext = document.querySelector('.main__slider-button-marker-arrow-right');
   buttonNextWrapper.style.backgroundColor = 'white';
   buttonNext.style.borderColor = '#ff7800';
 }
 
-function clickButtonUp() {
-  window.scrollTo(scrollY, 0);
-}
-
 function mouseenterButtonUp() {
-  var buttonUpWrapper = document.querySelector('.buttonUp-wrapper');
-  var arrowUp = document.querySelector('.buttonUp-marker-arrow-up');
+  var buttonUp = document.querySelector('.main__buttonUp');
+  var buttonMarkerArrowUp = document.querySelector('.main__button-marker-arrow-up');
 
-    buttonUpWrapper.style.backgroundColor = '#ff7800';
-    arrowUp.style.borderColor = 'white';
+    buttonUp.style.backgroundColor = '#ff7800';
+    buttonMarkerArrowUp.style.borderColor = 'white';
 }
 
 function mouseleaveButtonUp() {
-  var buttonUpWrapper = document.querySelector('.buttonUp-wrapper');
-  var arrowUp = document.querySelector('.buttonUp-marker-arrow-up');
+  var buttonUp = document.querySelector('.main__buttonUp');
+  var buttonMarkerArrowUp = document.querySelector('.main__button-marker-arrow-up');
   
-    buttonUpWrapper.style.backgroundColor = 'transparent';
-    buttonUpWrapper.style.borderColor = '#ff7800';
-    arrowUp.style.borderColor = '#ff7800';
+    buttonUp.style.backgroundColor = 'transparent';
+    buttonUp.style.borderColor = '#ff7800';
+    buttonMarkerArrowUp.style.borderColor = '#ff7800';
 }
 
 function offsetButtonSubscribeForm() {
-  var buttonSubscribeForm = document.querySelector('.subscribe-form__form-button');
+  var buttonSubscribeForm = document.querySelector('.main__subscribe-form-button');
   buttonSubscribeForm.style.transform = 'translateZ(-2px)';
 }
